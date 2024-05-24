cfb4133da577ab6d6230d1905309377d365ed377
Namrata <namrata.kesar10@gmail.com>
2024-05-24 16:17:58 +0530

    Correct the structure of user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,13 +3,8 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
+
 const registerUser = asyncHandler(async(req,res)=>{
-    
-    
-    
-    // return res
-
-
     // get user details from frontend
     const {fullName,username,email,password}=req.body;
     
